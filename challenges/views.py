27ca8b43cdc399c5dc1fb4be7259d01402672012
dzenankosuta <dzenan-kosuta@hotmail.com>
2025-01-05 07:39:57 +0100

    Add index view and update URL patterns for monthly challenges

--- challenges/views.py
+++ challenges/views.py
@@ -28,6 +28,15 @@ monthly_challenges = {
 # def march(request):
 #     return HttpResponse("Learn Django for at least 20 minutes every day!")
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        redirect_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{redirect_path}\">{month.capitalize()}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
@@ -39,6 +48,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
